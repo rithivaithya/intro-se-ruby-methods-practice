0f4865511e3f1547e51649ba60a4bd2f282351b6
Rithi Vaithyanathan <rithiv@gmail.com>
2017-08-08 15:44:51 +0000

    saved rb

---
+++ rubymethods.rb
@@ -0,0 +1,47 @@
+# def timesByOneThousand(number)
+#     return number*1000
+# end
+# puts timesByOneThousand(1)
+
+# def howOldWillIBeIn2030(number)
+#     return number+13
+# end
+# puts howOldWillIBeIn2030(17)
+
+# def makeEXCITED(word)
+#     return "word"
+# end
+# puts ("hello".upcase+"!!!")
+
+# def happyBackwords(word)
+#     return "word"
+# end
+# puts ("cool".reverse+":)")
+
+# def isEven(val)
+#     if val % 2 == 0
+#         return "#{val} is even"
+#     end
+    
+#     if val % 2 == 1
+#         return "#{val} is odd"
+#     end
+# end
+
+# puts isEven(897)
+
+#!/usr/bin/ruby
+
+# $age =  89
+# case $age
+# when 93..100
+#   puts "A"
+# when 90 .. 92
+#   puts "A-"
+# when 87.. 89
+#   puts "B+"
+# when 83 .. 86
+#   puts "B"
+# else
+#   puts "F"
+# end
